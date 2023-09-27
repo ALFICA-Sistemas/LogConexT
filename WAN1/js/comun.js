9e0f4d7a70e8f8fbe5999595a860e2d47d62c184
Sistemas (Admin) <alfica.sistemas@gmail.com>
2023-09-27 15:20:01 -0400

    Update del 2023-09-27_15:20

--- WAN1/js/comun.js
+++ WAN1/js/comun.js
@@ -1,8 +1,8 @@
-var Imagen = document.getElementById('LogGraf');
+// var Imagen = document.getElementById('LogGraf');
 
 function CargaImg(IDimagen, URLimagen){
    var tiempo = new Date();   // Leer el timestamp
-
+   var Imagen = document.getElementById(IDimagen);
    Imagen.src += '?'+tiempo;  // Usar el timestamp para hacer unico el URL de la imagen
 
    var tLocal = new Date(tiempo.getTime() - (tiempo.getTimezoneOffset() * 60000 )).toISOString();
